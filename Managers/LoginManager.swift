24949856237abf4f611291db0b1608e89dada5ac
Marko Mutavdzic <belikompijuter@gmail.com>
2023-03-09 13:46:56 +0100

    Setting headers

--- Managers/LoginManager.swift
+++ Managers/LoginManager.swift
@@ -14,28 +14,30 @@ protocol NetworkManager {
 
 protocol LoginManagerProtocol {
     var url: String { get set }
-    func login(email: String, password: String) -> AnyPublisher<LoginData, Error>
+    func login(email: String, password: String) -> AnyPublisher<NetwrokLogin, Error>
 }
 
 class LoginManager: LoginManagerProtocol {
     var url: String = "https://api-qa.menu.app/api/customers/login"
     
-    func login(email: String, password: String) -> AnyPublisher<LoginData, Error> {
+    func login(email: String, password: String) -> AnyPublisher<NetwrokLogin, Error> {
         let url = URL(string: url)!
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
-        request.setValue("application", forHTTPHeaderField: "mobile-application")
-        request.setValue("Device-UUID", forHTTPHeaderField: "123456")
-        request.setValue("Api-Version", forHTTPHeaderField: "3.7.0")
-        let authData = (email + ":" + password).data(using: .utf8)!.base64EncodedString()
-        request.addValue("Basic \(authData)", forHTTPHeaderField: "Authorization")
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("mobile-application", forHTTPHeaderField: "application")
         
+        // prepare json data
+        let json: [String: Any] = ["email": email,
+                                   "password": password]
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        request.httpBody = jsonData
     
         return URLSession.shared.dataTaskPublisher(for: request)
             .tryMap({ data, resposne in
                 return data
             })
-            .decode(type: LoginData.self, decoder: JSONDecoder())
+            .decode(type: NetwrokLogin.self, decoder: JSONDecoder())
             .eraseToAnyPublisher()
     }
 }
